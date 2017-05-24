b07a3b5e62d0cc8a1d05bf7c41d04967744de7f8
shadebane <jameswasson1@gmail.com>
2017-05-24 14:27:35 -0500

    finishing individual schema

--- modules/users/server/models/user.server.model.js
+++ modules/users/server/models/user.server.model.js
@@ -232,7 +232,103 @@ IndividualUserSchema.add({
       type: String,
       default: ''
     }
-  }
+  },
+  eudcation: [{
+    degreeLevel: {
+      type: String,
+      default: ''
+    },
+    schoolName: {
+      type: String,
+      default: ''
+    },
+    startDate: {
+      type: Date,
+      default: null
+    },
+    endDate: {
+      type: Date,
+      default: null
+    },
+    concentration: {
+      type: String,
+      default: ''
+    },
+    country: {
+      type: String,
+      default: ''
+    },
+    address: {
+      type: String,
+      default: ''
+    }
+  }],
+  jobExperience: [{
+    employer: {
+      type: String,
+      default: ''
+    },
+    title: {
+      type: String,
+      default: ''
+    },
+    description: {
+      type: String,
+      default: ''
+    },
+    skills: [{
+      type: String,
+      default: ''
+    }],
+    startDate: {
+      type: Date,
+      default: null
+    },
+    endDate: {
+      type: Date,
+      default: null
+    }
+  }],
+  certification: [{
+    name: {
+      type: String,
+      default: ''
+    },
+    organization: {
+      type: String,
+      default: ''
+    },
+    dateIssued: {
+      type: Date,
+      default: null
+    },
+    dateExpired: {
+      type: Date,
+      default: null
+    },
+    description: {
+      type: String,
+      default: ''
+    }
+  }],
+  skills: [{
+    skill: {
+      type: String,
+      default: ''
+    },
+    dateStart: {
+      type: Date,
+      default: null
+    },
+    dateEnd: {
+      type: Date,
+      default: null
+    },
+    locationLearned: [{
+      type: String,
+      default: ''
+    }]
+  }]
 });
 
 /**
