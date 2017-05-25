8a1fa172528849963690fc53bf3d7f0af863612d
GregoryBrinkman <GregoryLucasBrinkman@siu.edu>
2017-05-25 16:12:28 -0500

    Enterprise Controller Logic

--- modules/enterprises/client/controllers/list-enterprises.client.controller.js
+++ modules/enterprises/client/controllers/list-enterprises.client.controller.js
@@ -5,10 +5,13 @@
     .module('enterprises')
     .controller('EnterprisesListController', EnterprisesListController);
 
-  EnterprisesListController.$inject = ['EnterprisesService'];
+  EnterprisesListController.$inject = ['EnterprisesService', 'menuService'];
 
-  function EnterprisesListController(EnterprisesService) {
+  function EnterprisesListController(EnterprisesService, menuService) {
     var vm = this;
+
+    vm.menu = menuService.getMenu('enterprise');
+    console.log(vm.menu);
 
     vm.enterprises = EnterprisesService.query();
   }
