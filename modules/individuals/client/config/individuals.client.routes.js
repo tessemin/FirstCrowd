db872a4adc03f84527bae2e64459a1d06ceda016
markiragoldberg@outlook.com <markiragoldberg@outlook.com>
2017-05-25 10:01:27 -0500

    Set up routes for individuals' settings

--- modules/individuals/client/config/individuals.client.routes.js
+++ modules/individuals/client/config/individuals.client.routes.js
@@ -14,6 +14,61 @@
         url: '/individuals',
         template: '<ui-view/>'
       })
+      .state('individuals.profile', {
+        url: '/profile',
+        templateUrl: '/modules/individuals/client/views/settings/edit-profile.client.view.html',
+        controller: 'ProfileController',
+        controllerAs: 'vm',
+        data: {
+          pageTitle: 'Edit Your Account Details'
+        }
+      })
+      .state('individuals.bio', {
+        url: '/bio',
+        templateUrl: '/modules/individuals/client/views/settings/edit-bio.client.view.html',
+        controller: 'BioController',
+        controllerAs: 'vm',
+        data: {
+          pageTitle: 'Edit Your Personal Bio'
+        }
+      })
+      .state('individuals.certs', {
+        url: '/certifications',
+        templateUrl: '/modules/individuals/client/views/settings/edit-certs.client.view.html',
+        controller: 'CertsController',
+        controllerAs: 'vm',
+        data: {
+          pageTitle: 'Edit Your Certifications'
+        }
+      })
+      .state('individuals.education', {
+        url: '/education',
+        templateUrl: '/modules/individuals/client/views/settings/edit-education.client.view.html',
+        controller: 'EducationController',
+        controllerAs: 'vm',
+        data: {
+          pageTitle: 'Edit Your Education'
+        }
+      })
+      .state('individuals.experience', {
+        url: '/experience',
+        templateUrl: '/modules/individuals/client/views/settings/edit-experience.client.view.html',
+        controller: 'ExperienceController',
+        controllerAs: 'vm',
+        data: {
+          pageTitle: 'Edit Your Work Experience'
+        }
+      })
+      .state('individuals.skill', {
+        url: '/skill',
+        templateUrl: '/modules/individuals/client/views/settings/edit-skill.client.view.html',
+        controller: 'SkillController',
+        controllerAs: 'vm',
+        data: {
+          pageTitle: 'Edit your Skills'
+        }
+      })
+
       .state('individuals.list', {
         url: '',
         templateUrl: 'modules/individuals/client/views/list-individuals.client.view.html',
