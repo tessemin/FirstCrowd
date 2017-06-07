17736ba727b62c4f00f5e395cd9d9e64f03030e9
GregoryBrinkman <GregoryLucasBrinkman@siu.edu>
2017-06-07 11:49:18 -0500

    First data visualization working

--- modules/enterprises/client/controllers/graph.client.controller.js
+++ modules/enterprises/client/controllers/graph.client.controller.js
@@ -11,75 +11,17 @@
   function EnterpriseGraphController ($scope, $state, $window, Authentication, EnterprisesService, Notification) {
     var vm = this;
 
-    vm.text = 'Graph';
-
-    // vm.selected = {};
-    // vm.selectedURL = null;
-    // vm.selectedCompany = null;
-    // vm.selectedId = null;
-
-    // vm.saveGraph = saveGraph;
-    // vm.edit = edit;
-    // vm.delete = deleteItem;
-    // vm.cancel = cancel;
-
-    // createList();
-
-    // function edit(item) {
-    //   vm.company = item.companyName;
-    //   vm.selectedId = item._id;
-    //   vm.selectedURL = item.URL;
-    //   vm.selectedCompany = item.companyName;
-    // }
-
-    // function deleteItem(item) {
-    //   item.companyName = '';
-    //   item.URL = '';
-
-    //   EnterprisesService.updateGraphsFromForm(item)
-    //     .then(onUpdateGraphsSuccess)
-    //     .catch(onUpdateGraphsError);
-    // }
-
-    // function cancel() {
-    //   vm.selectedId = null;
-    //   vm.selectedURL = null;
-    //   vm.selectedCompany = null;
-    // }
-
-    // // UpdateGraph Enterprise
-    // function saveGraph(isValid) {
-
-    //   vm.selected._id = vm.selectedId;
-    //   vm.selected.URL = vm.selectedURL;
-    //   vm.selected.companyName = vm.selectedCompany;
-
-    //   if (!isValid) {
-    //     $scope.$broadcast('show-errors-check-validity', 'vm.competitorForm');
-    //     return false;
-    //   }
-
-    //   EnterprisesService.updateGraphsFromForm(vm.selected)
-    //     .then(onUpdateGraphsSuccess)
-    //     .catch(onUpdateGraphsError);
-    // }
-
-    // function onUpdateGraphsSuccess(response) {
-    //   Notification.success({ message: '<i class="glyphicon glyphicon-ok"></i> Graphs updated!' });
-    //   createList();
-    //   cancel();
-    // }
-
-    // function onUpdateGraphsError(response) {
-    //   Notification.error({ message: response.data.message, title: '<i class="glyphicon glyphicon-remove"></i> Update failed! Graphs not updated!' });
-    //   createList();
-    // }
-
-    // function createList() {
-    //   EnterprisesService.getEnterprise()
-    //     .then(function(response) {
-    //       vm.competitor = response.partners.competitor;
-    //     });
-    // }
+    vm.salesData = [
+      {hour: 1,sales: 54},
+      {hour: 2,sales: 66},
+      {hour: 3,sales: 77},
+      {hour: 4,sales: 70},
+      {hour: 5,sales: 60},
+      {hour: 6,sales: 63},
+      {hour: 7,sales: 55},
+      {hour: 8,sales: 47},
+      {hour: 9,sales: 55},
+      {hour: 10,sales: 30}
+    ];
   }
 }());
