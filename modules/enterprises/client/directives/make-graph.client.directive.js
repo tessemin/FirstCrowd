17bce0c2637630109e0d695a26af1b33af2b2d72
GregoryBrinkman <GregoryLucasBrinkman@siu.edu>
2017-06-12 11:27:01 -0500

    Horizontal Alignment for graph

--- modules/enterprises/client/directives/make-graph.client.directive.js
+++ modules/enterprises/client/directives/make-graph.client.directive.js
@@ -5,11 +5,14 @@
     .module('enterprises')
     .directive('makeGraph', makeGraph);
 
-  function makeGraph($window) {
+  makeGraph.$inject = ['$window', 'EnterprisesService'];
+
+  function makeGraph($window, EnterprisesService) {
     return {
       restrict: 'EA',
       scope: { data: '=' },
       link: function(scope, element, attrs) {
+
 
         var newsvg = d3.select("#graph").append("svg")
               .attr("width", 800)
@@ -23,18 +26,41 @@
         var globalY = height;
         var radius = 10;
 
-        var points = scope.data.nodes;
+        var g, g1, g2, g3;
+        getObject().then(function (obj) {
 
-        var g = svg.append("g")
+          var disp;
+          for (var item in obj){
+            if (item === 'supplier') {
+              disp = 200;
+            } else if (item === 'competitor') {
+              disp = 400;
+            } else if (item === 'customer') {
+              disp = 600;
+            }
+
+            g = svg.append("g")
               .selectAll("circle")
-              .data(points)
+              .data(item)
               .enter().append("circle")
-              .attr("cx", function(d) { return d.x; })
-              .attr("cy", function(d) { return getY(); })
+              .attr("cx", function(d) { return disp; })
+              .attr("cy", function(d) { return getY(item); })
               .attr("r", radius)
               .on("mouseover", handleMouseOver)
               .on("mouseout", handleMouseOut)
               .on("click", handleClick);
+
+            if (item === 'supplier') {
+              g1 = g;
+            } else if (item === 'competitor') {
+              g2 = g;
+            } else if (item === 'customer') {
+              g3 = g;
+            }
+
+            globalY = height;
+          }
+        });
 
         function handleMouseOver(d, i) {
           d3.select(this).attr('r', radius * 2).style('fill', 'orange');
@@ -45,7 +71,7 @@
             x: function() { return xScale(d.x) - 30; },
             y: function() { return yScale(d.y) - 15; }
           })
-            .text([1, 2, 3]);
+            .text('hi');
           // d3.select(this).append("text").attr('x', ).attr('y', )
           //   .text(function() {
           //     return 'hi';
@@ -58,8 +84,8 @@
           d3.select("#t" + d.x + "-" + d.y + "-" + i).remove();  // Remove text location
         }
 
-        function getY() {
-          var len = points.length + 1,
+        function getY(item) {
+          var len = item.length + 1,
               pixels = height / len;
 
           globalY = globalY - pixels;
@@ -77,7 +103,9 @@
                  .on("zoom", zoomed));
 
         function zoomed() {
-          g.attr("transform", d3.event.transform);
+          g1.attr("transform", d3.event.transform);
+          g2.attr("transform", d3.event.transform);
+          g3.attr("transform", d3.event.transform);
         }
 
         function getObject() {
