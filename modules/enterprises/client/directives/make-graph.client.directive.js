5ca173cb0808b1235f05a54b07258e5db52b32dd
GregoryBrinkman <GregoryLucasBrinkman@siu.edu>
2017-06-14 15:05:16 -0500

    Main company node centered on load

--- modules/enterprises/client/directives/make-graph.client.directive.js
+++ modules/enterprises/client/directives/make-graph.client.directive.js
@@ -21,28 +21,37 @@
             width = +svg.attr("width"),
             height = +svg.attr("height"),
             transform = d3.zoomIdentity,
-            globalY = height,
+            globalY = 0,
             radius = 10;
 
         var g1, g2, g3, c1, c2, c3, t1, t2, t3;
-
+        var rootNode = {}, flag = false;
         getObject().then(function (obj) {
-          var disp, y;
-          for (var item in obj){
+          var disp, y, list = obj;
+          for (var item in list) {
             if (item === 'supplier') {
               disp = 100;
-            } else if (item === 'competitor') {
+            }
+            else if (item === 'competitor') {
+              var len = list[item].length;
+              var mid = Math.ceil(len/2);
+              list[item].splice(mid-1, 0, rootNode);
               disp = 300;
-            } else if (item === 'customer') {
+            }
+            else if (item === 'customer') {
               disp = 500;
+            }
+
+            if (item === 'competitor') {
+              flag = true;
             }
 
             var g = svg.append("g")
                   .selectAll("rect")
-                  .data(obj[item])
+                  .data(list[item])
                   .enter().append("rect")
                   .attr("x", function(d) { return disp; })
-                  .attr("y", function(d) { return getY(obj, item); })
+                  .attr("y", function(d) { return getY(list, item, flag); })
                   .attr("rx", 5)
                   .attr("ry", 5)
                   .attr("width", 90)
@@ -54,25 +63,33 @@
                   .on("mouseout", handleMouseOut)
                   .on("click", handleClick);
 
+            if (item === "competitor") {
+              flag = true;
+            }
+
             var c = svg.append("g")
                   .selectAll("circle")
-                  .data(obj[item])
+                  .data(list[item])
                   .enter().append("circle")
                   .attr("cx", function(d) { return disp + 10; })
-                  .attr("cy", function(d) { return getY(obj, item) + 10; })
+                  .attr("cy", function(d) { return getY(list, item, flag) + 10; })
                   .attr("r", 10)
                   .attr("fill", "white")
                   .attr("stroke", "black")
                   .attr("stroke-width", 2);
 
+            if (item === "competitor") {
+              flag = true;
+            }
+
             var t = svg.append("g")
                   .selectAll("text")
-                  .data(obj[item])
+                  .data(list[item])
                   .enter().append("text")
                   .text(function(d) { return d.companyName; })
                   .attr("font-size", "4")
                   .attr("x", function(d) { return disp + 2; })
-                  .attr("y", function(d) { return getY(obj, item) + 25; })
+                  .attr("y", function(d) { return getY(list, item, flag) + 25; })
                   .attr("dx", "0")
                   .attr("dy", "0")
                   .call(wrap, 70);
@@ -93,6 +110,37 @@
           }
         });
 
+        function handleMouseOut(d, i) {
+          // d3.select(this).attr('r', radius).style('fill', 'steelblue');
+          d3.select('#tooltip').remove();  // Remove text location
+        }
+
+        function getY(obj, item) {
+          var addedLen = 1;
+
+          if(item === "competitor" && obj[item].len % 2 === 0) {
+            addedLen += 1;
+          }
+
+          var len = obj[item].length + addedLen,
+              pixels = height / len;
+
+          globalY = globalY + pixels;
+
+          if (flag) {
+            globalY = globalY + pixels/2;
+          }
+          flag = false;
+
+          if (globalY + pixels >= height) {
+            var y = globalY;
+            globalY = 0;
+            return y - 50;
+          }
+          console.log(globalY);
+          return globalY - 50;
+        }
+
         function handleMouseOver(d, i) {
           // d3.select(this).attr('r', radius * 2).style('fill', 'orange');
           var tooltip = d3.select('body').append('div')
@@ -108,25 +156,6 @@
             .style('left',(d3.event.pageX - 35) + 'px')
             .style('top', (d3.event.pageY - 30) + 'px');
         }
-
-        function handleMouseOut(d, i) {
-          // d3.select(this).attr('r', radius).style('fill', 'steelblue');
-          d3.select('#tooltip').remove();  // Remove text location
-        }
-
-        function getY(obj, item) {
-          var len = obj[item].length + 1,
-              pixels = height / len;
-
-          globalY = globalY - pixels;
-          if (globalY - pixels === 0){
-            var y = globalY;
-            globalY = height;
-            return y;
-          }
-          return globalY;
-        }
-
 
         function handleClick() {
 
@@ -151,12 +180,13 @@
         function getObject() {
           return EnterprisesService.getEnterprise()
             .then(function(response) {
+              rootNode.companyName = response.profile.companyName;
+              rootNode.URL = response.profile.URL;
               return response.partners;
             });
         }
 
         function wrap(text, width) {
-          console.log(text);
           text.each(function() {
             var text = d3.select(this),
                 words = text.text().split(/\s+/).reverse(),
