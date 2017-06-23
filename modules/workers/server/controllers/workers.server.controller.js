67e57900b45f6c783afd3d9e2c57bfcfa15f6ef4
shadebane <jameswasson1@gmail.com>
2017-06-23 09:09:04 -0500

    adding function ality for worker

--- modules/workers/server/controllers/workers.server.controller.js
+++ modules/workers/server/controllers/workers.server.controller.js
@@ -9,6 +9,5 @@ var _ = require('lodash');
  * Extend workers's controller
  */
 module.exports = _.extend(
-  require('./workers/workers.server.controller'),
   require('./workers/workers.tasks.server.controller')
 );
--- modules/workers/server/controllers/workers/workers.tasks.server.controller.js
+++ modules/workers/server/controllers/workers/workers.tasks.server.controller.js
@@ -13,7 +13,8 @@ var path = require('path'),
   enterpriseControler = require(path.resolve('./modules/enterprises/server/controllers/enterprises.server.controller')),
   _ = require('lodash');
   
-var workerWhitelistedFields = ['progress'];
+var workerWhitelistedFields = ['progress'],
+  taskId = null;
   
   
 /*
