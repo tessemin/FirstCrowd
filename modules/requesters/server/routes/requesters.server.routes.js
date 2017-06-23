fb71fbc8f1074d9592ede177666c1dde405d904d
shadebane <jameswasson1@gmail.com>
2017-06-23 11:04:24 -0500

    removing broken routes for push

--- modules/requesters/server/routes/requesters.server.routes.js
+++ modules/requesters/server/routes/requesters.server.routes.js
@@ -16,6 +16,44 @@ module.exports = function(app) {
     .get(requesters.read)
     .put(requesters.update)
     .delete(requesters.delete);
+    
+  /*
+   * REQUESTER TABLE
+   */
+/*   
+  // ACTIVE TASKS
+  app.route('/api/requesters/activeTask/').put(requesters.activeTask.update)
+  app.route('/api/requesters/activeTask/all').post(requesters.activeTask.all);
+  app.route('/api/requesters/activeTask/add').post(requesters.activeTask.add);
+  
+  // SUSPENDED TASKS
+  app.route('/api/requesters/suspendedTask/').put(requesters.suspendedTask.update);
+  app.route('/api/requesters/suspendedTask/all').post(requesters.suspendedTask.all);
+  app.route('/api/requesters/suspendedTask/add').post(requesters.suspendedTask.add);
+  
+  // COMPLETED TASKS
+  app.route('/api/requesters/completedTask/').put(requesters.completedTask.update);
+  app.route('/api/requesters/completedTask/all').post(requesters.completedTask.all);
+  app.route('/api/requesters/completedTask/add').post(requesters.completedTask.add);
+  
+  // REJECTED TASKS
+  app.route('/api/requesters/rejectedTask/').put(requesters.rejectedTask.update);
+  app.route('/api/requesters/rejectedTask/all').post(requesters.rejectedTask.all);
+  app.route('/api/requesters/rejectedTask/add').post(requesters.rejectedTask.add);
+  
+  // RATINGS
+  app.route('/api/requesters/workerRating/').put(requesters.workerRating.update);
+  app.route('/api/requesters/workerRating/all').post(requesters.workerRating.all);
+  app.route('/api/requesters/workerRating/add').post(requesters.workerRating.add);
+  
+  // MISC INFORMATION
+  app.route('/api/requesters/getRequesterData/').post(requesters.getRequesterData.all); */
+  
+  /*
+   * TASK TABLE
+   */
+  
+  // CREATE A NEW TASK
 
   // Finish by binding the Requester middleware
   app.param('requesterId', requesters.requesterByID);
