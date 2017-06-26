d5e61556cb21af146a0ad579546274a3a95033fd
markiragoldberg@outlook.com <markiragoldberg@outlook.com>
2017-06-26 14:03:47 -0500

    fix skills comma space

--- modules/individuals/client/controllers/Experience.client.controller.js
+++ modules/individuals/client/controllers/Experience.client.controller.js
@@ -13,7 +13,7 @@
     vm.experiences = [];
     vm.addExperience = addExperience;
     vm.removeExperience = removeExperience;
-    
+
     IndividualsService.getIndividual().$promise
       .then(function(data) {
         var work = data.jobExperience;
@@ -22,20 +22,20 @@
           vm.experiences[i].employer = work[i].employer;
           vm.experiences[i].jobTitle = work[i].jobTitle;
           vm.experiences[i].description = work[i].description;
-          vm.experiences[i].skills = work[i].skills;
+          vm.experiences[i].skills = work[i].skills.join(', ');
           vm.experiences[i].startDate = new Date(work[i].startDate);
           vm.experiences[i].endDate = new Date(work[i].endDate);
         }
       });
-    
+
     function addExperience() {
       vm.experiences.push({});
     }
-    
+
     function removeExperience(index) {
       vm.experiences.splice(index, 1);
     }
-    
+
     vm.updateIndividualExperience = updateIndividualExperience;
 
     // Update a user profile
@@ -46,16 +46,16 @@
         Notification.error({ message: 'Fill out required fields!' });
         return false;
       }
-      
+
       IndividualsService.updateExperienceFromForm(vm.experiences)
         .then(onUpdateExperiencesSuccess)
         .catch(onUpdateExperiencesError);
     }
-    
+
     function onUpdateExperiencesSuccess(response) {
       Notification.success({ message: '<i class="glyphicon glyphicon-ok"></i> Work Experience updated!' });
     }
-    
+
     function onUpdateExperiencesError(response) {
       Notification.error({ message: response.data.message, title: '<i class="glyphicon glyphicon-remove"></i> Update failed! Work Experience not updated!' });
     }
