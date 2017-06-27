60059095d749a8198d7c250581630810b0d0ff20
GregoryBrinkman <GregoryLucasBrinkman@siu.edu>
2017-06-27 10:39:46 -0500

    Created Requester Routes, added a semicolon

--- modules/requesters/client/services/requesters.client.service.js
+++ modules/requesters/client/services/requesters.client.service.js
@@ -12,10 +12,66 @@
     return $resource('api/requesters/:requesterId', {
       requesterId: '@_id'
     }, {
+      // ALL REQUESTER TASKS
+      _getAllTasks:{
+        method: 'POST',
+        url: '/api/requesters/tasks/all'
+      },
+      // ACTIVE TASKS
+      _updateActiveTasks: {
+        method: 'PUT',
+        url: '/api/requesters/activeTask/update'
+      },
+      _getActiveTasks: {
+        method: 'POST',
+        url: '/api/requesters/activeTask/all'
+      },
+      _addActiveTask: {
+        method: 'POST',
+        url: '/api/requesters/activeTask/add'
+      },
+      // SUSPENDED TASKS
+      _updateSuspendedTask: {
+        method: 'PUT',
+        url: '/api/requesters/suspendedTask/update'
+      },
+      _getSuspendedTasks: {
+        method: 'POST',
+        url: '/api/requesters/suspendedTask/all'
+      },
+      _addSuspendedTask: {
+        method: 'POST',
+        url: '/api/requesters/suspendedTask/add'
+      },
+      // COMPLETED TASKS
+      _updateCompletedTask: {
+        method: 'PUT',
+        url: '/api/requesters/completedTask/update'
+      },
+      _getCompletedTasks: {
+        method: 'POST',
+        url: '/api/requesters/completedTask/all'
+      },
+      _addCompletedTask: {
+        method: 'POST',
+        url: '/api/requesters/completedTask/add'
+      },
       // REJECTED TASKS
+      _updateRejectedTask: {
+        method: 'PUT',
+        url: '/api/requesters/rejectedTask/update'
+      },
+      _getRejectedTasks: {
+        method: 'POST',
+        url: '/api/requesters/rejectedTask/all'
+      },
+      _addRejectedTask: {
+        method: 'POST',
+        url: '/api/requesters/rejectedTask/add'
+      },
       // RATINGS
       _updateWorkerRating: {
-        method: 'POST',
+        method: 'PUT',
         url: '/api/requesters/workerRating/update'
       },
       _getWorkerRatings: {
@@ -45,12 +101,56 @@
         url: '/api/tasks/deleteTask'
       },
       _updateTask: {
-        method: 'POST',
+        method: 'PUT',
         url: '/api/tasks/updateTask'
       }
     });
-    
+
     angular.extend(Requesters, {
+      // ALL REQUESTER TASKS
+      getAllTasks: function() {
+        return this._getAllTasks().$promise;
+      },
+      // ACTIVE TASKS
+      updateActiveTasks: function() {
+        return this._updateActiveTasks().$promise;
+      },
+      getActiveTasks: function() {
+        return this._getActiveTasks().$promise;
+      },
+      addActiveTask: function() {
+        return this._addActiveTask().$promise;
+      },
+      // SUSPENDED TASKS
+      updateSuspendedTask: function() {
+        return this._updateSuspendedTask().$promise;
+      },
+      getSuspendedTasks: function() {
+        return this._getSuspendedTasks().$promise;
+      },
+      addSuspendedTask: function() {
+        return this._addSuspendedTask().$promise;
+      },
+      // COMPLETED TASKS
+      updateCompletedTask: function() {
+        return this._updateCompletedTask().$promise;
+      },
+      getCompletedTasks: function() {
+        return this._getCompletedTasks().$promise;
+      },
+      addCompletedTask: function() {
+        return this._addCompletedTask().$promise;
+      },
+      // REJECTED TASKS
+      updateRejectedTask: function() {
+        return this._updateRejectedTask().$promise;
+      },
+      getRejectedTasks: function() {
+        return this._getRejectedTasks().$promise;
+      },
+      addRejectedTask: function() {
+        return this._addRejectedTask().$promise;
+      },
       // RATINGS
       updateWorkerRating: function () {
         return this._updateWorkerRating().$promise;
--- modules/requesters/server/routes/requesters.server.routes.js
+++ modules/requesters/server/routes/requesters.server.routes.js
@@ -6,41 +6,41 @@
 var requestersPolicy = require('../policies/requesters.server.policy'),
   requesters = require('../controllers/requesters.server.controller');
 
-module.exports = function(app) { 
+module.exports = function(app) {
   /*
    * REQUESTER TABLE
-   */ 
+   */
   // ALL REQUESTER TASKS
   app.route('/api/requesters/tasks/all').post(requesters.tasks.all);
   // ACTIVE TASKS
-  app.route('/api/requesters/activeTask/update').put(requesters.activeTask.update)
+  app.route('/api/requesters/activeTask/update').put(requesters.activeTask.update);
   app.route('/api/requesters/activeTask/all').post(requesters.activeTask.all);
   app.route('/api/requesters/activeTask/add').post(requesters.activeTask.add);
-  
+
   // SUSPENDED TASKS
   app.route('/api/requesters/suspendedTask/update').put(requesters.suspendedTask.update);
   app.route('/api/requesters/suspendedTask/all').post(requesters.suspendedTask.all);
   app.route('/api/requesters/suspendedTask/add').post(requesters.suspendedTask.add);
-  
+
   // COMPLETED TASKS
   app.route('/api/requesters/completedTask/update').put(requesters.completedTask.update);
   app.route('/api/requesters/completedTask/all').post(requesters.completedTask.all);
   app.route('/api/requesters/completedTask/add').post(requesters.completedTask.add);
-  
+
   // REJECTED TASKS
   app.route('/api/requesters/rejectedTask/update').put(requesters.rejectedTask.update);
   app.route('/api/requesters/rejectedTask/all').post(requesters.rejectedTask.all);
   app.route('/api/requesters/rejectedTask/add').post(requesters.rejectedTask.add);
-  
+
   // RATINGS
   app.route('/api/requesters/workerRating/update').put(requesters.workerRating.update);
   app.route('/api/requesters/workerRating/all').post(requesters.workerRating.all);
   app.route('/api/requesters/workerRating/create').post(requesters.workerRating.create);
-  
+
   // REQUESTER INFORMATION
   app.route('/api/requesters/getRequesterData').post(requesters.getRequesterData.all);
   app.route('/api/requesters/getRequesterRatings').post(requesters.getRequesterData.ratings);
-  
+
   /*
    * TASK TABLE
    */
