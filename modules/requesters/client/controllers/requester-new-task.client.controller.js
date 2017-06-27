dd5b0bc6cefc24bf6a45ad745c2617385e0ca2c3
markiragoldberg@outlook.com <markiragoldberg@outlook.com>
2017-06-27 10:05:01 -0500

    new task form closer

--- modules/requesters/client/controllers/requester-new-task.client.controller.js
+++ modules/requesters/client/controllers/requester-new-task.client.controller.js
@@ -11,7 +11,13 @@
   function RequesterNewTaskController ($scope, $http, $state, IndividualsService, Authentication, Notification) {
     var vm = this;
 
-    vm.newTask = {};
+    vm.newTask = {
+      preapproval: true,
+      payment: {
+        // has to be defined as 'false' for an ng-if to work on page load
+        bidding: false
+      }
+    };
 
 
     console.log(vm.newTask.name);
