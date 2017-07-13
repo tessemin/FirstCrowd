cb6615e40644ee38e66732c65af2b5b5a96527cd
shadebane <jameswasson1@gmail.com>
2017-07-13 10:40:26 -0500

    removing old routes from enterprise

--- modules/enterprises/server/routes/enterprises.server.routes.js
+++ modules/enterprises/server/routes/enterprises.server.routes.js
@@ -7,15 +7,6 @@ var enterprisesPolicy = require('../policies/enterprises.server.policy'),
   enterprises = require('../controllers/enterprises.server.controller');
 
 module.exports = function(app) {
-  // Enterprises Routes
-  app.route('/api/enterprises').all(enterprisesPolicy.isAllowed)
-    .get(enterprises.list)
-    .post(enterprises.create);
-
-  app.route('/api/enterprises/:enterpriseId').all(enterprisesPolicy.isAllowed)
-    .get(enterprises.read)
-    .put(enterprises.update)
-    .delete(enterprises.delete);
     
   app.route('/enterprises/api/enterprises/profile/').post(enterprises.updateProfile);
   
