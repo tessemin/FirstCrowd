2b720c51e9f31258a7d242dfc8c743a714f1c313
markiragoldberg@outlook.com <markiragoldberg@outlook.com>
2017-07-17 11:00:50 -0500

    tweak resume

--- modules/individuals/client/controllers/ResumeController.client.controller.js
+++ modules/individuals/client/controllers/ResumeController.client.controller.js
@@ -12,10 +12,13 @@
     var vm = this;
     vm.user = Authentication.user;
     vm.individual = {};
-    
+
     IndividualsService.getIndividual().$promise
       .then(function(data) {
         vm.individual = data;
+        vm.individual.bio.profession = vm.individual.bio.profession.replace(/,(?=[^\s])/g, ", ");
+        vm.user.phone = vm.user.phone.toString();
+        vm.user.phone = vm.user.phone.slice(0,3) + '-' + vm.user.phone.slice(3,6) + '-' + vm.user.phone.slice(6)
       })
       .catch(function(data) {
         Notification.error({ message: data.message, title: 'Error retrieving resume data' });
